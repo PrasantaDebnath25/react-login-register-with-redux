d53402866bb3926ba468981fdefb600b2fe3652f
PrasantaDebnath25 <prasantadebnath.2013@gmail.com>
2023-06-24 17:00:24 +0530

    code format changed

--- src/Component/Layout/Header.js
+++ src/Component/Layout/Header.js
@@ -4,8 +4,8 @@ import Login from '../Login'
 import Counter from '../Pages/Counter'
 import './HeaderStyle.css'
 import imgs from './bar.png'
-import { useState, useEffect } from 'react'   
-import { Button,Modal } from 'react-bootstrap'
+import { useState, useEffect } from 'react'
+import { Button, Modal } from 'react-bootstrap'
 import Registration from '../Registration'
 import Timer from '../Timer'
 import { useHistory } from "react-router-dom";
@@ -15,45 +15,45 @@ function Header() {
     const [modal, setmodal] = useState(false);
     localStorage.setItem("modalValue", modal);
     const [buttonClick, setbuttonClick] = useState("");
-    return(
+    return (
         <div>
-           <div class="topnav">
-                    <a ><img src={imgs} alt="image" height="25px" width="30px"/></a>
-                
+            <div class="topnav">
+                <a ><img src={imgs} alt="image" height="25px" width="30px" /></a>
+
                 <div class="topnav-centered">
                     <a href="/" class="active">This is new Project</a>
-                    
+
                 </div>
                 {/* <a href="/">Home</a> */}
                 <div class="topnav-right">
-                    {localStorage.getItem("status") === 'true' 
-                        && <a> <Timer show='on' /> </a> }
-                    {   localStorage.getItem("status") === 'true' 
+                    {localStorage.getItem("status") === 'true'
+                        && <a> <Timer show='on' /> </a>}
+                    {localStorage.getItem("status") === 'true'
                         // && <a onClick={() => {
                         //     history.push('/ShowData')
                         //     }}
                         //     >Profile</a>
-                        
-                        
-                            && <a onClick={() => {
-                            localStorage.setItem("status",'false')
+
+
+                        && <a onClick={() => {
+                            localStorage.setItem("status", 'false')
                             console.log("Logout Sucessfully")
                             history.push('/')
-                        {<Timer show='off' /> }
-                            }}
+                            { <Timer show='off' /> }
+                        }}
                         >   Logout</a>
-                        
-                        
+
+
                         || <a onClick={() => {
                             setmodal(true); setbuttonClick("login");
-                            }}
-                            >Login</a>
+                        }}
+                        >Login</a>
                     }
-                    {   localStorage.getItem("status") === 'true' 
+                    {localStorage.getItem("status") === 'true'
                         && <a onClick={() => {
                             history.push('/ShowData')
-                            }}
-                            >Profile</a>
+                        }}
+                        >Profile</a>
                         || <a onClick={() => {
                             setmodal(true); setbuttonClick("reg");
                         }}
@@ -63,47 +63,48 @@ function Header() {
                             setmodal(true); setbuttonClick("login");
                         }}
                         >Login</a> */}
-                    
+
                     {/* <a href="/Registration">Registration</a> */}
-                
+
                 </div>
-                
+
             </div>
             <div >
-            
-            
+
+
             </div>
-            <Modal style={{ top: "10%",
-  left: "20%",
-    // justify-content: center,
+            <Modal style={{
+                top: "10%",
+                left: "20%",
+                // justify-content: center,
                 backgroundImage: `url("https://picsum.photos/id/1067/200/300")`,
-                width:'850px',height:'650px'
-             }} show={modal} onHide={() => setmodal(false)} >
-              
-            {buttonClick === "login" ?
+                width: '850px', height: '650px'
+            }} show={modal} onHide={() => setmodal(false)} >
+
+                {buttonClick === "login" ?
                     <p className='font-small grey-text d-flex justify-content-end'>
-                          Have not an account? Go to
-                          <a className='blue-text ml-1' onClick={() => {
+                        Have not an account? Go to
+                        <a className='blue-text ml-1' onClick={() => {
                             setbuttonClick("reg");
-                        } }>
-                           <h5><b> Register</b></h5>
-                          </a>
-                        </p>
-                        :  <p className='font-small grey-text d-flex justify-content-end'>
+                        }}>
+                            <h5><b> Register</b></h5>
+                        </a>
+                    </p>
+                    : <p className='font-small grey-text d-flex justify-content-end'>
                         Have an account? Go to
                         <a className='blue-text ml-1' onClick={() => {
-                          setbuttonClick("login");
-                      } }>
-                         <h5><b>Log in</b></h5>
+                            setbuttonClick("login");
+                        }}>
+                            <h5><b>Log in</b></h5>
                         </a>
-                      </p>
+                    </p>
                 }
                 {buttonClick === "login" ?
-                <Login /> :
-                <Registration />
+                    <Login /> :
+                    <Registration />
                 }
-                   
-                </Modal>
+
+            </Modal>
         </div>
     )
 }
