927e1c78c2dc3a000a7087f53b2ee786973380a5
PrasantaDebnath25 <prasantadebnath.2013@gmail.com>
2023-06-24 16:59:32 +0530

    Change code

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'   
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Header from './Layout/Header'
 import Login from './Login'
@@ -10,48 +10,50 @@ import { connect } from 'react-redux'
 import { fetchUsers } from '../Redux/user/userActions'
 import FooterTeam from './Pages/Footer/FooterTeam'
 import Footer from './Pages/Footer/Footer'
-    function Home ({ userData, fetchUsers }) {
-        useEffect(() => {
-          fetchUsers() 
-        }, [])
-        return (
-            <div style={{
-                // backgroundImage: `url("https://www.nicesnippets.com/image/imgpsh_fullsize.png")`
-             }}> 
-                <Header /> 
-               
-                { userData.loading ?
-                    ( <h2>Loading</h2> ) : 
-                    userData.error ? 
-                       ( <h2>{userData.error}</h2> ) : (
-                    <div>
-                        <div className="flex-style2">
-                        { userData && userData.users && userData.users.map(data => {  
-                            return  <div className="flex-style2" key={ data.id }> {
-                                <Card names={ data.name } id={data.id} mail={data.email} add={ data.address.street } city={ data.address.city } zipcode={ data.address.zipcode } phn={ data.phone } website={ data.website } />} </div>;
-                            // <div className="flex-style2" key={ data.id }> {<Card id={data.id} mail={data.email} add={ data.address.street } city={ data.address.city } zipcode={ data.address.zipcode } phn={ data.phone } website={ data.website } />}
-                            })
-                        }
-                        </div>
-                    </div>
-                    ) 
-                }
-                <FooterTeam />
-                <Footer />
-            </div>
-        )
-      }
 
-    const mapStateToProps = state => {
-        return {
-            userData: state.user
-        }
+
+function Home({ userData, fetchUsers }) {
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+    return (
+        <div style={{
+            // backgroundImage: `url("https://www.nicesnippets.com/image/imgpsh_fullsize.png")`
+        }}>
+            <Header />
+
+            {userData.loading ?
+                (<h2>Loading</h2>) :
+                userData.error ?
+                    (<h2>{userData.error}</h2>) : (
+                        <div>
+                            <div className="flex-style2">
+                                {userData && userData.users && userData.users.map(data => {
+                                    return <div className="flex-style2" key={data.id}> {
+                                        <Card names={data.name} id={data.id} mail={data.email} add={data.address.street} city={data.address.city} zipcode={data.address.zipcode} phn={data.phone} website={data.website} />} </div>;
+                                    // <div className="flex-style2" key={ data.id }> {<Card id={data.id} mail={data.email} add={ data.address.street } city={ data.address.city } zipcode={ data.address.zipcode } phn={ data.phone } website={ data.website } />}
+                                })
+                                }
+                            </div>
+                        </div>
+                    )
+            }
+            <FooterTeam />
+            <Footer />
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        userData: state.user
     }
-  
-    const mapDispatchToProps = dispatch => {
-        return {
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
         fetchUsers: () => dispatch(fetchUsers())
-        }
     }
-  
-export default connect( mapStateToProps, mapDispatchToProps )(Home)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
